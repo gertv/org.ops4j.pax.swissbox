d8e17803bbbf055458a281c1b483d5a61b092e99
Gert Vanthienen <gert.vanthienen@gmail.com>
2011-01-20 14:55:56 +0100

    Avoid 'Bundle could not be generated' WARN message when InputStream is being partially read

--- pax-swissbox-bnd/src/main/java/org/ops4j/pax/swissbox/bnd/BndUtils.java
+++ pax-swissbox-bnd/src/main/java/org/ops4j/pax/swissbox/bnd/BndUtils.java
@@ -25,7 +25,9 @@ import java.io.PipedOutputStream;
 import java.io.UnsupportedEncodingException;
 import java.net.MalformedURLException;
 import java.net.URLDecoder;
+import java.nio.channels.Pipe;
 import java.util.Properties;
+import java.util.concurrent.atomic.AtomicBoolean;
 import java.util.jar.Manifest;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
@@ -155,7 +157,7 @@ public class BndUtils
     private static PipedInputStream createInputStream( final Jar jar )
         throws IOException
     {
-        final PipedInputStream pin = new PipedInputStream();
+        final CloseAwarePipedInputStream pin = new CloseAwarePipedInputStream();
         final PipedOutputStream pout = new PipedOutputStream( pin );
 
         new Thread()
@@ -168,7 +170,15 @@ public class BndUtils
                 }
                 catch( IOException e )
                 {
-                    LOG.warn( "Bundle cannot be generated" );
+                    if (pin.closed)
+                    {
+                        // logging the exception at DEBUG logging instead
+                        // -- reading thread probably stopped reading
+                        LOG.debug( "Bundle cannot be generated", e );
+                    }
+                    else {
+                        LOG.warn( "Bundle cannot be generated", e );
+                    }
                 }
                 finally
                 {
@@ -292,4 +302,19 @@ public class BndUtils
         throw exception;
     }
 
+
+    /**
+     * PipedInputStream implementation that keeps track of whether it has been closed or not.
+     */
+    private static final class CloseAwarePipedInputStream extends PipedInputStream
+    {
+
+        private boolean closed = false;
+
+        public void close() throws IOException
+        {
+            closed = true;
+            super.close();
+        }
+    }
 }
